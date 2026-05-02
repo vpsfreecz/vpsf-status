6caa77298e8051abe9590e8022987270d1659d0a
Jakub Skokan <jakub.skokan@havefun.cz>
2026-05-02 15:47:53 +0200

    Replace shell.nix with a flake dev shell
    The development environment depended on shell.nix, which does not pin nixpkgs
    and does not support the flake workflow used by newer Nix setups.
    
    Add flake.nix and flake.lock with an equivalent devShell that provides Go and
    GNU Make, then remove the legacy shell.nix entry point.

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "vpsFree.cz status development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              gnumake
+              go
+            ];
+
+            shellHook = ''
+              if [ -n "$PS1" ]; then
+                dev_shell_prompt="(vpsf-status) "
+                case "$PS1" in
+                  "$dev_shell_prompt"*|\\n"$dev_shell_prompt"*) ;;
+                  \\n*) PS1="\\n$dev_shell_prompt''${PS1#\\n}" ;;
+                  *) PS1="$dev_shell_prompt$PS1" ;;
+                esac
+                export PS1
+                unset dev_shell_prompt
+              fi
+            '';
+          };
+        });
+    };
+}
